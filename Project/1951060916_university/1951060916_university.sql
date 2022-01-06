ce43105a158094f078d3741ae3ceb92681c0e97d
kevilphu <1951060916@e.tlu.edu.vn>
2022-01-06 17:56:42 +0700

    ảnh src và sql

---
+++ Project/1951060916_university/1951060916_university.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 06, 2022 lúc 11:43 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `ktgk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `monhoc`
+--
+
+CREATE TABLE `monhoc` (
+  `mamh` int(11) NOT NULL,
+  `ten_mh` varchar(50) DEFAULT NULL,
+  `sotinchi` int(11) DEFAULT NULL,
+  `sotiet_lt` int(11) DEFAULT NULL,
+  `sotiet_bt` int(11) DEFAULT NULL,
+  `sotiet_thtn` int(11) DEFAULT NULL,
+  `sogio_tuhoc` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `monhoc`
+--
+
+INSERT INTO `monhoc` (`mamh`, `ten_mh`, `sotinchi`, `sotiet_lt`, `sotiet_bt`, `sotiet_thtn`, `sogio_tuhoc`) VALUES
+(0, 'toan', 12, 33, 44, 10, 20),
+(1, 'CTS-V', 44, 22, 92, 49, 19),
+(2, 'Toronado', 4, 44, 73, 28, 12),
+(3, 'Xterra', 29, 23, 3, 70, 46),
+(4, 'Accord', 81, 17, 34, 58, 13),
+(5, 'Passat', 36, 45, 77, 76, 20);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `monhoc`
+--
+ALTER TABLE `monhoc`
+  ADD PRIMARY KEY (`mamh`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
